11e470effabae0a25eb88906e48f80b9af99f0d2
IslamElzneny <eslam.elzneny@creatifyteam.com>
2023-12-31 22:01:51 +0200

    design navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,25 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { FormErrorsComponent } from './components/form-errors/form-errors.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    FormErrorsComponent
+    FormErrorsComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
     MatFormFieldModule,
+    MatToolbarModule,
+    MatButtonModule,
     SharedRoutingModule
   ],
   exports:[
-    FormErrorsComponent
+    FormErrorsComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
